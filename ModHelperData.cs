f49c430a5f216553314bb09ff5acd5c88c4090f7
James Gale <obsidiantoolzg@gmail.com>
2022-08-09 21:31:06 -0700

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -5,7 +5,7 @@ public static class ModHelperData
     public const string Version = "1.0.0";
     public const string Name = "BTD6+";
 
-    public const string Description = "Adds the plus sign in the logo from Blooons TD6 + and hides microtransaction " +
+    public const string Description = "(Meme) Adds the plus sign in the logo from Blooons TD6 + and hides microtransaction " +
                                       "buttons for that authentic Apple Arcade experience right on your own computer!";
 
     public const string RepoOwner = "doombubbles";
